4e062a2bb48597185a788a0eddfee7901e20a754
kenneth <kenneth.nepsther@gmail.com>
2021-02-17 19:38:35 +0100

    primeira aula(tipos primitivos)

---
+++ src/tiposPrimitivos/TiposPrimitivos.java
@@ -0,0 +1,38 @@
+package tiposPrimitivos;
+
+public class TiposPrimitivos {
+
+	public static void main(String[]args) {
+		/**
+         * int idade = 23;
+            float numero = 10.000f;
+            long longo = 1000000000000000000L;
+            double valor = 20.00;
+            char letra = '\u0041'; //char letra = 'A'; //char letra = 89;
+            boolean verdadeiro = true;
+            boolean falso = false;
+            String Resultado = "kenneth Luzolo";
+            System.out.println("O numero é: "+letra);
+         */
+		//=================== Exercicios 1======================================
+	       String nome = "Kenneth Luzolo";
+	       int idade = 102;
+	       float peso = 94.4f;
+	       float altura = 1.79f;
+	       String Endereco = "Benfica|Zona verde| rua 12";
+	       String telefone = "934260018";
+	       
+	        System.out.println("Nome: "+nome+" Idade: "+idade+" Peso: "+peso+" altura: "
+	                             +altura+" Endereço: "+Endereco+" Telefone: "+telefone);       
+	       
+	//=================== Exercicios 2======================================
+	       char Sexo = 'M';
+	       //float Salario = 157.000f;
+	       //String EstadoCivil = "Casado";
+	              
+	       System.out.println("Nome: "+Sexo +" Idade: "+idade+" Peso: "+peso+" altura: "
+	                             +altura+" Endereço: "+Endereco+" Telefone: "+telefone);  
+		
+		
+	}
+}
